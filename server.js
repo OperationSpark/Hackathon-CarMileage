e766a9df55d88eefcce9f35ef0b218227553243a
Shams Ali <ali.s90@outlook.com>
2017-03-15 22:25:45 -0500

    (add) firebase auth

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+// initialize firebase
+var firebase = require('firebase');
+var config = {
+  apiKey: "AIzaSyBoUPlAe5Jbz_lc20sluF8IW8GQyvj2vjM",
+  authDomain: "project--3096610514914531623.firebaseapp.com",
+  databaseURL: "https://project--3096610514914531623.firebaseio.com",
+  storageBucket: "project--3096610514914531623.appspot.com",
+  messagingSenderId: "640777191644"
+};
+var firebaseApp = firebase.initializeApp(config);
+var db = firebaseApp.database();
+
+console.log(firebase.app().name);  // "[DEFAULT]"
+
+
+console.log(firebaseApp);
 // Define the port to run on
 app.set('port', 3000);
 

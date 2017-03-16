694ab3d62d07c7a16834ace8e3232877ee8df990
Shams Ali <ali.s90@outlook.com>
2017-03-15 22:59:58 -0500

    (add) auth

--- public/app.js
+++ public/app.js
@@ -50,3 +50,51 @@ app.controller('MainCtrl', function($scope, $document) {
     });
   }
 });
+
+app.controller('AuthCtrl', function($scope, $document){
+  $scope.isLoggedIn = false;
+  // FirebaseUI config.
+  var uiConfig = {
+    callbacks: {
+      signInSuccess: function(currentUser, credential, redirectUrl) {
+        $scope.isLoggedIn = true;
+        // Do something.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        // return true;
+      },
+      uiShown: function() {
+        console.log()
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM,
+    // Query parameter name for mode.
+    queryParameterForWidgetMode: 'mode',
+    // Query parameter name for sign in success url.
+    queryParameterForSignInSuccessUrl: 'signInSuccessUrl',
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: 'localhost:3000',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        // Whether the display name should be displayed in the Sign Up page.
+        requireDisplayName: true
+      }
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>'
+  };
+
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
+
+})

a090ece447a2fcf69b9239e0dcda8b7c760db5d9
Shams Ali <ali.s90@outlook.com>
2017-03-16 23:09:55 -0500

    (update) user is able to add data to firebase

--- public/app.js
+++ public/app.js
@@ -1,7 +1,32 @@
 var app = angular.module('plunker', ['google-maps']);
 
 app.controller('MainCtrl', function($scope, $document) {
-  // map object
+  var currentUser;
+  //functions to write user data
+  function writeUserData(userId, name, email, directionRequest) {
+    cosnole.log('Writing Data To Firebase')
+    firebase.database().ref('users/' + userId).set({
+      username: name,
+      email: email,
+      directionRequest: directionRequest
+    });
+  }
+
+  // Check If User Is Signed In
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log(user, 'user is signed in');
+      currentUser = user;
+      $scope.isLoggedIn = true;
+    } else {
+      console.log('user is signed out');
+      currentUser = null;
+      $scope.isLoggedIn = false;
+    }
+  }, function(error) {
+    console.log(error);
+  });
+
   $scope.map = {
     control: {},
     center: {
@@ -40,6 +65,9 @@ app.controller('MainCtrl', function($scope, $document) {
     };
     directionsService.route(request, function (response, status) {
       if (status === google.maps.DirectionsStatus.OK) {
+        console.log(response, 'direction service success response');
+        console.log(currentUser.uid, currentUser.displayName, currentUser.email, response.request),
+        writeUserData(currentUser.uid, currentUser.displayName, currentUser.email, response.request);
         directionsDisplay.setDirections(response);
         directionsDisplay.setMap($scope.map.control.getGMap());
         directionsDisplay.setPanel(document.getElementById('directionsList'));
@@ -49,52 +77,5 @@ app.controller('MainCtrl', function($scope, $document) {
       }
     });
   }
+
 });
-
-app.controller('AuthCtrl', function($scope, $document){
-  $scope.isLoggedIn = false;
-  // FirebaseUI config.
-  var uiConfig = {
-    callbacks: {
-      signInSuccess: function(currentUser, credential, redirectUrl) {
-        $scope.isLoggedIn = true;
-        // Do something.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        // return true;
-      },
-      uiShown: function() {
-        console.log()
-        // The widget is rendered.
-        // Hide the loader.
-        document.getElementById('loader').style.display = 'none';
-      }
-    },
-    credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM,
-    // Query parameter name for mode.
-    queryParameterForWidgetMode: 'mode',
-    // Query parameter name for sign in success url.
-    queryParameterForSignInSuccessUrl: 'signInSuccessUrl',
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: 'localhost:3000',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      {
-        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        // Whether the display name should be displayed in the Sign Up page.
-        requireDisplayName: true
-      }
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>'
-  };
-
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  // The start method will wait until the DOM is loaded.
-  ui.start('#firebaseui-auth-container', uiConfig);
-
-})

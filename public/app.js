ea365ffe8b9e7a5948758a3e5bfbc3a731da4e54
George Mauer <gmauer@gmail.com>
2017-03-18 17:48:59 +0000

    very basic ui

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('plunker', ['google-maps']);
+var app = angular.module('plunker', []);
 
 app.controller('MainCtrl', function($scope, $document) {
   var currentUser;
@@ -26,55 +26,5 @@ app.controller('MainCtrl', function($scope, $document) {
   }, function(error) {
     console.log(error);
   });
-
-  $scope.map = {
-    control: {},
-    center: {
-        latitude: -37.812150,
-        longitude: 144.971008
-    },
-    zoom: 14
-  };
-
-  // marker object
-  $scope.marker = {
-    center: {
-        latitude: -37.812150,
-        longitude: 144.971008
-    }
-  }
-
-  // instantiate google map objects for directions
-  var directionsDisplay = new google.maps.DirectionsRenderer();
-  var directionsService = new google.maps.DirectionsService();
-  var geocoder = new google.maps.Geocoder();
-
-  // directions object -- with defaults
-  $scope.directions = {
-    origin: "Collins St, Melbourne, Australia",
-    destination: "MCG Melbourne, Australia",
-    showList: false
-  }
-
-  // get directions using google maps api
-  $scope.getDirections = function () {
-    var request = {
-      origin: $scope.directions.origin,
-      destination: $scope.directions.destination,
-      travelMode: google.maps.DirectionsTravelMode.DRIVING
-    };
-    directionsService.route(request, function (response, status) {
-      if (status === google.maps.DirectionsStatus.OK) {
-        console.log(response, 'direction service success response');
-        writeUserData(currentUser.uid, currentUser.displayName, currentUser.email, response.request);
-        directionsDisplay.setDirections(response);
-        directionsDisplay.setMap($scope.map.control.getGMap());
-        directionsDisplay.setPanel(document.getElementById('directionsList'));
-        $scope.directions.showList = true;
-      } else {
-        alert('Google route unsuccesfull!');
-      }
-    });
-  }
 
 });
--- server.js
+++ server.js
@@ -3,23 +3,23 @@ var path = require('path');
 var app = express();
 
 // initialize firebase
-var firebase = require('firebase');
-var config = {
-  apiKey: "AIzaSyBoUPlAe5Jbz_lc20sluF8IW8GQyvj2vjM",
-  authDomain: "project--3096610514914531623.firebaseapp.com",
-  databaseURL: "https://project--3096610514914531623.firebaseio.com",
-  storageBucket: "project--3096610514914531623.appspot.com",
-  messagingSenderId: "640777191644"
-};
-var firebaseApp = firebase.initializeApp(config);
-var db = firebaseApp.database();
+// var firebase = require('firebase');
+// var config = {
+//   apiKey: "AIzaSyBoUPlAe5Jbz_lc20sluF8IW8GQyvj2vjM",
+//   authDomain: "project--3096610514914531623.firebaseapp.com",
+//   databaseURL: "https://project--3096610514914531623.firebaseio.com",
+//   storageBucket: "project--3096610514914531623.appspot.com",
+//   messagingSenderId: "640777191644"
+// };
+// var firebaseApp = firebase.initializeApp(config);
+// var db = firebaseApp.database();
 
-console.log(firebase.app().name);  // "[DEFAULT]"
+// console.log(firebase.app().name);  // "[DEFAULT]"
 
 
-console.log(firebaseApp);
+// console.log(firebaseApp);
 // Define the port to run on
-app.set('port', 3000);
+app.set('port', process.env.PORT);
 
 app.use(express.static(path.join(__dirname, 'public')));
 

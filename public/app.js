a6ae7d41c031849e68bc7bc0f38a35e553a06318
George Mauer <gmauer@gmail.com>
2017-03-18 18:12:20 +0000

    view for each car item tracking

--- public/app.js
+++ public/app.js
@@ -1,30 +1,49 @@
 var app = angular.module('plunker', []);
 
-app.controller('MainCtrl', function($scope, $document) {
-  var currentUser;
-  //functions to write user data
-  function writeUserData(userId, name, email, directionRequest) {
-    console.log('Writing Data To Firebase')
-    firebase.database().ref('users/' + userId).set({
-      username: name,
-      email: email,
-      directionRequest: directionRequest
-    });
-  }
+const lpad = (str, padWith, minLength) =>
+  str.length >= minLength ? str : lpad(padWith+str, padWith, minLength);
+const rand2Hex = () => lpad(Math.floor(Math.random()*256).toString(16), '0', 2);
+this.randomColor = () => `#${rand2Hex()}${rand2Hex()}${rand2Hex()}`;
+    
 
-  // Check If User Is Signed In
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      console.log(user, 'user is signed in');
-      currentUser = user;
-      $scope.isLoggedIn = true;
-    } else {
-      console.log('user is signed out');
-      currentUser = null;
-      $scope.isLoggedIn = false;
+app.controller('MainCtrl', [function() {
+  this.cars = [
+    {
+      name: "My Honda",
+      odometer: 10000,
+      tracking: [
+        { name: "Oil Change", percent: 60, color: randomColor() },
+        { name: "Tire Rotation", percent: 32, color: randomColor() },
+        { name: "Tire Change", percent: 75, color: randomColor() },
+        { name: "Windshield Fluid", percent: 7, color: randomColor() },
+      ],
     }
-  }, function(error) {
-    console.log(error);
-  });
+  ];
+  
+  // var currentUser;
+  //functions to write user data
+  // function writeUserData(userId, name, email, directionRequest) {
+  //   console.log('Writing Data To Firebase')
+  //   firebase.database().ref('users/' + userId).set({
+  //     username: name,
+  //     email: email,
+  //     directionRequest: directionRequest
+  //   });
+  // }
 
-});
+  // // Check If User Is Signed In
+  // firebase.auth().onAuthStateChanged(function(user) {
+  //   if (user) {
+  //     console.log(user, 'user is signed in');
+  //     currentUser = user;
+  //     $scope.isLoggedIn = true;
+  //   } else {
+  //     console.log('user is signed out');
+  //     currentUser = null;
+  //     $scope.isLoggedIn = false;
+  //   }
+  // }, function(error) {
+  //   console.log(error);
+  // });
+
+}]);
